267c024acaf6d0fa344a2ef86cca3c4571527986
Shivam Singh <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2026-05-17 12:58:46 +0530

    filling buffer with stream of data user provides

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::TcpListener;
-
+use std::io::{Read, Write};
 fn main() {
     // 1. Bind to a port (Open the front door)
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -9,8 +9,26 @@ fn main() {
     // 2. Wait for clients to connect
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            // making stream mutable because we will need to read from it later
+            Ok(mut stream) => {
                 println!("A new client connected!");
+
+                // make a bucket to store buffer sent by user in terminal
+                let mut buffer = [0; 512];
+
+                // 4. Read the data from the stream into our bucket
+                // stream.read gives us the number of bytes read, we will use that to know how much of the buffer is actually filled with data
+                // and the data in buffer filled by stream.read is the command sent by user in terminal
+                match stream.read(&mut buffer){
+                    Ok(size) => {
+                        // 5. Translate the raw bytes into a String
+                        let command = String::from_utf8_lossy(&buffer[..size]);
+                        println!("Received command: {}", command);
+                    }
+                    Err(e) => {
+                        println!("Failed to read data: {}", e);
+                    }
+                }
             }
             Err(e) => {
                 println!("Failed to connect: {}", e);
