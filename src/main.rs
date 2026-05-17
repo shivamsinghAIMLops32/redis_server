6413f7fcfb38ad7222a29f6b6cd87345b2f50ede
Shivam Singh <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2026-05-17 13:13:33 +0530

    included thread for every connected user so our system can responde back

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::net::TcpListener;
 use std::io::{Read, Write};
+use std::net::TcpListener;
+use std::thread;
 fn main() {
     // 1. Bind to a port (Open the front door)
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     println!("Mini-Redis is listening on port 6379!");
 
     // 2. Wait for clients to connect
@@ -13,35 +14,35 @@ fn main() {
             Ok(mut stream) => {
                 println!("A new client connected!");
 
+                thread::spawn(move || {
+                    loop {
+                        // make a bucket to store buffer sent by user in terminal
+                        let mut buffer = [0; 512];
 
-                loop{
-                // make a bucket to store buffer sent by user in terminal
-                let mut buffer = [0; 512];
+                        // 4. Read the data from the stream into our bucket
+                        // stream.read gives us the number of bytes read, we will use that to know how much of the buffer is actually filled with data
+                        // and the data in buffer filled by stream.read is the command sent by user in terminal
+                        match stream.read(&mut buffer) {
+                            Ok(size) => {
+                                // 3. Handle the client disconnecting
+                                if size == 0 {
+                                    println!("Client disconnected.");
+                                    break;
+                                }
 
-                // 4. Read the data from the stream into our bucket
-                // stream.read gives us the number of bytes read, we will use that to know how much of the buffer is actually filled with data
-                // and the data in buffer filled by stream.read is the command sent by user in terminal
-                match stream.read(&mut buffer){
-                    Ok(size) => {
-                        // 3. Handle the client disconnecting
-                            if size == 0 {
-                                println!("Client disconnected.");
-                                break;
+                                // 5. Translate the raw bytes into a String
+                                let command = String::from_utf8_lossy(&buffer[..size]);
+                                println!("Received command: {}", command);
+
+                                // 4. Send a response back!
+                                stream.write_all(b"+PONG\r\n").unwrap();
                             }
-
-
-                        // 5. Translate the raw bytes into a String
-                        let command = String::from_utf8_lossy(&buffer[..size]);
-                        println!("Received command: {}", command);
-
-                        // 4. Send a response back!
-                            stream.write_all(b"+PONG\r\n").unwrap();
+                            Err(e) => {
+                                println!("Failed to read data: {}", e);
+                            }
+                        }
                     }
-                    Err(e) => {
-                        println!("Failed to read data: {}", e);
-                    }
-                }
-                }
+                });
             }
             Err(e) => {
                 println!("Failed to connect: {}", e);
